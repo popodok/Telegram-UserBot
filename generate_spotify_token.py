97a30643ac8a121557651b4e3ac5b117731e6905
PolisanTheEasyNick <39007846+PolisanTheEasyNick@users.noreply.github.com>
2021-08-08 12:28:10 +0300

    Update generate_spotify_token.py

--- generate_spotify_token.py
+++ generate_spotify_token.py
@@ -7,6 +7,6 @@ scopes = "user-read-playback-state"
 clientId = input("Input client id: ")
 secretId = input("Input secret id: ")
 redirect = input("Input redirect URI: ")
-sp = SpotifyOAuth(client_id=clientId, client_secret=secretId, redirect_uri="http://example.com", scope=scopes, cache_path=getcwd() + "/sp_token")
+sp = SpotifyOAuth(client_id=clientId, client_secret=secretId, redirect_uri=redirect, scope=scopes, cache_path=getcwd() + "/sp_token")
 sp.get_access_token()
 print("Done")
